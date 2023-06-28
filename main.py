2733709c9010645be68a5bc55e23c4f6dc78ba1e
DandinPower <tomhot246@gmail.com>
2023-06-28 11:48:14 +0800

    update

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
 import gradio as gr
 import os
 import openai
-from prompt import GetBaseArticlePrompt, GetBaseArticlePromptByPDF, GetQuestionPrompt, GetSystemPrompt
+from prompt import GetBaseArticlePrompt, GetQuestionPrompt, GetSystemPrompt
 from dotenv import load_dotenv
 load_dotenv()
-API_KEY = os.getenv("OPENAI_API_KEY")
-ARTICLE_PATH = os.getenv("ARTICLE_PATH")
+API_KEY = os.getenv("OPENAI_API")
 ARTICLE_TXT_PATH = os.getenv("ARTICLE_TXT_PATH")
 openai.api_key = API_KEY
 
 def generate_answer(question):
-
     systemPrompt = GetSystemPrompt()
     baseArticlePrompt = GetBaseArticlePrompt(ARTICLE_TXT_PATH)
-    # baseArticlePrompt = GetBaseArticlePromptByPDF(ARTICLE_PATH)
     questionPrompt = GetQuestionPrompt(question)
     userPrompt = f"{baseArticlePrompt}\n{questionPrompt}"
 
     completion = openai.ChatCompletion.create(
         model="gpt-3.5-turbo-16k",
+        # model="gpt-4",
         messages=[
             {"role": "system", "content": systemPrompt},
             {"role": "user", "content": userPrompt}
@@ -32,5 +30,11 @@ def greet(question):
     answer = generate_answer(question)
     return answer
 
-iface = gr.Interface(fn=greet, inputs="text", outputs="text")
-iface.launch()
+# Create the interface
+def greet2(question):
+    answer = generate_answer(question)
+    return answer
+
+iface1 = gr.Interface(fn=greet, inputs="text", outputs="text")
+
+iface1.launch()
